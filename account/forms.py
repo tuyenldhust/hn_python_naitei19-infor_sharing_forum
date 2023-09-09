4a8fc33ff275c11994db8ab0f4c7aa3fbb805949
tuyenldhust <tuyen.ld194715@sis.hust.edu.vn>
2023-09-09 21:38:21 +0700

    add reset password

--- account/forms.py
+++ account/forms.py
@@ -1,5 +1,6 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, PasswordResetForm, SetPasswordForm
 from django import forms
+from django.contrib.auth import authenticate
 
 from app.models import CustomUser
 
@@ -11,3 +12,24 @@ class SignUpForm(UserCreationForm):
 class SignInForm(forms.Form):
     username = forms.CharField(max_length=30)
     password = forms.CharField(max_length=30, widget=forms.PasswordInput)
+
+    def clean(self):
+        username = self.cleaned_data.get('username')
+        password = self.cleaned_data.get('password')
+        user = authenticate(username=username, password=password)
+
+        try:
+            if not user.is_active:
+                raise forms.ValidationError("Tài khoản của bạn chưa được kích hoạt, vui lòng kiểm tra email!")
+        except:
+            raise forms.ValidationError("Tài khoản hoặc mật khẩu không đúng!")
+        return self.cleaned_data
+
+class PasswordResetForm(PasswordResetForm):
+    def __init__(self, *args, **kwargs):
+        super(PasswordResetForm, self).__init__(*args, **kwargs)
+
+class SetPasswordForm(SetPasswordForm):
+    class Meta:
+        model = CustomUser
+        fields = ['new_password1', 'new_password2']
--- account/views.py
+++ account/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .forms import SignUpForm, SignInForm
+from .forms import SignUpForm, SignInForm, PasswordResetForm, SetPasswordForm
 from django.shortcuts import redirect
 from django.contrib.auth import login, authenticate, logout
 from django.contrib import messages
@@ -9,6 +9,7 @@ from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.utils.encoding import force_bytes, force_str
 from django.core.mail import EmailMessage
 from django.contrib.auth import get_user_model
+from django.db.models.query_utils import Q
 import re
 
 from .tokens import account_activation_token
@@ -93,3 +94,65 @@ def signout(request):
     logout(request)
     messages.success(request, 'Đăng xuất thành công!')
     return redirect('index')
+
+def password_reset_request(request):
+    if request.user.is_authenticated:
+        messages.warning(request, 'Bạn đang đăng nhập!!!')
+        return redirect('index')
+    
+    if request.method == 'POST':
+        form = PasswordResetForm(request.POST)
+        if form.is_valid():
+            user_email = form.cleaned_data['email']
+            associated_user = get_user_model().objects.filter(Q(email=user_email)).first()
+            if associated_user:
+                subject = "Đặt lại mật khẩu"
+                message = render_to_string("account/template_reset_password.html", {
+                    'user': associated_user,
+                    'domain': get_current_site(request).domain,
+                    'uid': urlsafe_base64_encode(force_bytes(associated_user.pk)),
+                    'token': account_activation_token.make_token(associated_user),
+                    "protocol": 'https' if request.is_secure() else 'http'
+                })
+                email = EmailMessage(subject, message, to=[associated_user.email])
+                if email.send():
+                    messages.success(request, "Chúng tôi đã gửi email để reset mật khẩu của bạn")
+                else:
+                    messages.error(request, "Đã có lỗi xảy ra trong quá trình gửi mail. Vui lòng thử lại sau.")
+                return redirect("index")
+            else:
+                messages.error(request, "Không có tài khoản nào được liên kết với email này")
+        else:
+            for error in list(form.errors.values()):
+                messages.error(request, clean_message(error))
+    else:
+        form = PasswordResetForm()
+    
+    return render(request=request, template_name="account/password_reset.html", context={"form":form})
+
+def passwordResetConfirm(request, uidb64, token):
+    User = get_user_model()
+    try:
+        uid = force_str(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except:
+        user = None
+
+    if user is not None and account_activation_token.check_token(user, token):
+        if request.method == 'POST':
+            form = SetPasswordForm(user, request.POST)
+            if form.is_valid():
+                form.save()
+                messages.success(request, "Đặt lại mật khẩu thành công. Bạn có thể đăng nhập bằng mật khẩu mới.")
+                return redirect('signin')
+            else:
+                for error in list(form.errors.values()):
+                    messages.error(request, clean_message(error))
+        else:
+            form = SetPasswordForm(user)
+            return render(request, 'account/password_reset_confirm.html', {'form': form})
+    else:
+        messages.error(request, "Liên kết đặt lại mật khẩu không hợp lệ hoặc đã hết hạn!")
+
+    messages.error(request, 'Đã có lỗi xảy ra trong quá trình đặt lại mật khẩu. Vui lòng thử lại sau.')
+    return redirect("index")
