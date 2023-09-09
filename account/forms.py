a909ccd4963455d4c8e52dba765b1c0703b5461e
tuyenldhust <tuyen.ld194715@sis.hust.edu.vn>
2023-09-09 21:38:21 +0700

    Task #67379: [Guess] Đăng nhập

--- account/forms.py
+++ account/forms.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
 from django.contrib.auth import authenticate
+from datetime import datetime
+from django.utils.translation import gettext_lazy as _
 
 from app.models import CustomUser
 
@@ -8,3 +10,24 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = CustomUser
         fields = ('first_name', 'last_name', 'email', 'username')
+
+class SignInForm(forms.Form):
+    username = forms.CharField(max_length=30)
+    password = forms.CharField(max_length=30, widget=forms.PasswordInput)
+
+    def clean(self):
+        username = self.cleaned_data.get('username')
+        password = self.cleaned_data.get('password')
+        user = authenticate(username=username, password=password)
+
+        if user is None:
+            raise forms.ValidationError(_("Tài khoản không tồn tại hoặc sai tên đăng nhập hoặc sai mật khẩu!"))
+        else:
+            if not user.is_active and user.count_violated < 3:
+                raise forms.ValidationError(_("Tài khoản của bạn chưa được kích hoạt, vui lòng kiểm tra email!"))
+            elif not user.is_active and user.count_violated >= 3:
+                raise forms.ValidationError(_("Tài khoản của bạn đã bị cấm vĩnh viễn!"))
+            elif user.is_active and user.count_violated < 3 and (datetime.now().date() - user.time_banned.date()).days < 14:
+                raise forms.ValidationError(_("Tài khoản của bạn đã bị khóa trong 14 ngày!"))
+
+        return self.cleaned_data
